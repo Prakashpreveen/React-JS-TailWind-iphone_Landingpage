a704e320290051f853907d2d8b44881a5ddf2283
Divya Prakash <prakashpreveen@gmail.com>
2024-12-22 01:28:12 +0530

    added 3D view

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,37 +1,55 @@
 import { useGSAP } from "@gsap/react";
-import React, { useRef, useState } from "react";
-import { gsap } from "gsap";
+import gsap from "gsap";
 import ModelView from "./ModelView";
-import { color } from "three/tsl";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
   const [size, setSize] = useState("small");
   const [model, setModel] = useState({
-    title: "iPhone Pro in Natural Titanium",
-    color: ["#8F8A81", "#FFE789", "#6F6C64"],
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
   });
 
-  //camera control for the model view:
+  // camera control for the model view
   const cameraControlSmall = useRef();
   const cameraControlLarge = useRef();
 
-  //model:
+  // model
   const small = useRef(new THREE.Group());
   const large = useRef(new THREE.Group());
 
-  //rotation
+  // rotation
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
+
   useGSAP(() => {
-    gsap.to("#heading", { opacity: 1, y: 0 });
+    gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
 
   return (
@@ -46,7 +64,8 @@ const Model = () => {
             <ModelView
               index={1}
               groupRef={small}
-              gsapType={cameraControlSmall}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
               setRotationState={setSmallRotation}
               item={model}
               size={size}
@@ -55,7 +74,8 @@ const Model = () => {
             <ModelView
               index={2}
               groupRef={large}
-              gsapType={cameraControlLarge}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
               setRotationState={setLargeRotation}
               item={model}
               size={size}
@@ -67,8 +87,8 @@ const Model = () => {
                 position: "fixed",
                 top: 0,
                 bottom: 0,
-                right: 0,
                 left: 0,
+                right: 0,
                 overflow: "hidden",
               }}
               eventSource={document.getElementById("root")}
@@ -98,7 +118,7 @@ const Model = () => {
                     key={label}
                     className="size-btn"
                     style={{
-                      backgroundColor: size === value ? "white" : "black",
+                      backgroundColor: size === value ? "white" : "transparent",
                       color: size === value ? "black" : "white",
                     }}
                     onClick={() => setSize(value)}
